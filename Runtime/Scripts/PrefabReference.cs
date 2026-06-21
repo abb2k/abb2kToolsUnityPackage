9bb27c37ee621a556a3801aed8c9b921d62a8010
abb2k <75845401+abb2k@users.noreply.github.com>
2026-06-21 17:40:07 +0300

    prefab reference with params

--- Runtime/Scripts/PrefabReference.cs
+++ Runtime/Scripts/PrefabReference.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [System.Serializable]
-public class PrefabReference<T> where T : MonoBehaviour
+public abstract class PrefabReferenceBase<T> where T : MonoBehaviour
 {
     [SerializeField]
     private T _component;
@@ -13,143 +13,86 @@ public class PrefabReference<T> where T : MonoBehaviour
     public T Component => _component;
     public GameObject GameObject => _component != null ? _component.gameObject : null;
 
-    public static implicit operator T(PrefabReference<T> reference) => reference?.Component;
-    
-    public Object InstantiateObject(Vector3 position, Quaternion rotation)
-    {
-        return Object.Instantiate(GameObject, position, rotation);
-    }
+    public Object InstantiateObject(Vector3 position, Quaternion rotation) => Object.Instantiate(GameObject, position, rotation);
+    public Object InstantiateObject(Vector3 position, Quaternion rotation, Transform parent) => Object.Instantiate(GameObject, position, rotation, parent);
+    public Object InstantiateObject() => Object.Instantiate(GameObject);
+    public Object InstantiateObject(Scene scene) => Object.Instantiate(GameObject, scene);
+    public Object InstantiateObject(Transform parent) => Object.Instantiate(GameObject, parent, false);
+    public Object InstantiateObject(Transform parent, bool instantiateInWorldSpace) => Object.Instantiate(GameObject, parent, instantiateInWorldSpace);
 
-    public Object InstantiateObject(Vector3 position, Quaternion rotation, Transform parent)
-    {
-        return Object.Instantiate(GameObject, position, rotation, parent);
-    }
-
-    public Object InstantiateObject()
-    {
-        return Object.Instantiate(GameObject);
-    }
-
-    public Object InstantiateObject(Scene scene)
-    {
-        return Object.Instantiate(GameObject, scene);
-    }
-
-    public T Instantiate(InstantiateParameters parameters)
-    {
-        return Object.Instantiate<T>(Component, parameters);
-    }
-
-    public T Instantiate(Vector3 position, Quaternion rotation, InstantiateParameters parameters)
-    {
-        return Object.Instantiate<T>(Component, position, rotation, parameters);
-    }
-
-    public Object InstantiateObject(Transform parent)
-    {
-        return Object.Instantiate(GameObject, parent, false);
-    }
-
-    public Object InstantiateObject(Transform parent, bool instantiateInWorldSpace)
-    {
-        return Object.Instantiate(GameObject, parent, instantiateInWorldSpace);
-    }
-
-    public T Instantiate()
-    {
-        return Object.Instantiate(Component);
-    }
-
-    public T Instantiate(Vector3 position, Quaternion rotation)
-    {
-        return Object.Instantiate<T>(Component, position, rotation);
-    }
-
-    public T Instantiate(Vector3 position, Quaternion rotation, Transform parent)
-    {
-        return Object.Instantiate<T>(Component, position, rotation, parent);
-    }
-
-    public T Instantiate(Transform parent)
-    {
-        return Object.Instantiate<T>(Component, parent, worldPositionStays: false);
-    }
-
-    public T Instantiate(Transform parent, bool worldPositionStays)
-    {
-        return Object.Instantiate<T>(Component, parent, worldPositionStays);
-    }
+    protected T InstantiateInternal(InstantiateParameters parameters) => Object.Instantiate<T>(Component, parameters);
+    protected T InstantiateInternal(Vector3 position, Quaternion rotation, InstantiateParameters parameters) => Object.Instantiate<T>(Component, position, rotation, parameters);
+    protected T InstantiateInternal() => Object.Instantiate<T>(Component);
+    protected T InstantiateInternal(Vector3 position, Quaternion rotation) => Object.Instantiate<T>(Component, position, rotation);
+    protected T InstantiateInternal(Vector3 position, Quaternion rotation, Transform parent) => Object.Instantiate<T>(Component, position, rotation, parent);
+    protected T InstantiateInternal(Transform parent) => Object.Instantiate<T>(Component, parent, false);
+    protected T InstantiateInternal(Transform parent, bool worldPositionStays) => Object.Instantiate<T>(Component, parent, worldPositionStays);
 
 #if UNITY_EDITOR
+    /// <summary>
+    /// EDITOR ONLY! Instantiates a packed prefab as a raw Object
+    /// </summary>
+    public Object InstantiatePrefabObject() => PrefabUtility.InstantiatePrefab(GameObject);
+    public Object InstantiatePrefabObject(Scene destinationScene) => PrefabUtility.InstantiatePrefab(GameObject, destinationScene);
+    public Object InstantiatePrefabObject(Transform parent) => PrefabUtility.InstantiatePrefab(GameObject, parent);
 
     /// <summary>
-    /// EDITOR ONLY!
-    /// 
-    /// Instantiates a packed prefab
+    /// EDITOR ONLY! Instantiates a packed prefab as type T
     /// </summary>
-    /// <returns></returns>
-    public UnityEngine.Object InstantiatePrefabObject()
+    protected T InstantiatePrefabInternal() => (PrefabUtility.InstantiatePrefab(GameObject) as GameObject)?.GetComponent<T>();
+    protected T InstantiatePrefabInternal(Scene destinationScene) => (PrefabUtility.InstantiatePrefab(GameObject, destinationScene) as GameObject)?.GetComponent<T>();
+    protected T InstantiatePrefabInternal(Transform parent) => (PrefabUtility.InstantiatePrefab(GameObject, parent) as GameObject)?.GetComponent<T>();
+#endif
+}
+
+[System.Serializable]
+public class PrefabReference<T> : PrefabReferenceBase<T> where T : MonoBehaviour, IInitializable
+{
+    private T Initialize(T instance)
     {
-        return PrefabUtility.InstantiatePrefab(GameObject);
+        if (instance != null)
+            instance.Init();
+            
+        return instance;
     }
 
-    /// <summary>
-    /// EDITOR ONLY!
-    /// 
-    /// Instantiates a packed prefab
-    /// </summary>
-    /// <returns></returns>
-    public UnityEngine.Object InstantiatePrefabObject(Scene destinationScene)
+    public T Instantiate() => Initialize(InstantiateInternal());
+    public T Instantiate(Vector3 position, Quaternion rotation) => Initialize(InstantiateInternal(position, rotation));
+    public T Instantiate(Vector3 position, Quaternion rotation, Transform parent) => Initialize(InstantiateInternal(position, rotation, parent));
+    public T Instantiate(Transform parent) => Initialize(InstantiateInternal(parent));
+    public T Instantiate(Transform parent, bool worldPositionStays) => Initialize(InstantiateInternal(parent, worldPositionStays));
+    public T Instantiate(InstantiateParameters parameters) => Initialize(InstantiateInternal(parameters));
+    public T Instantiate(Vector3 position, Quaternion rotation, InstantiateParameters parameters) => Initialize(InstantiateInternal(position, rotation, parameters));
+
+#if UNITY_EDITOR
+    public T InstantiatePrefab() => Initialize(InstantiatePrefabInternal());
+    public T InstantiatePrefab(Scene destinationScene) => Initialize(InstantiatePrefabInternal(destinationScene));
+    public T InstantiatePrefab(Transform parent) => Initialize(InstantiatePrefabInternal(parent));
+#endif
+}
+
+[System.Serializable]
+public class PrefabReference<T, D> : PrefabReferenceBase<T> where T : MonoBehaviour, IInitializable<D>
+{
+    private T Initialize(T instance, D data)
     {
-        return PrefabUtility.InstantiatePrefab(GameObject, destinationScene);
+        if (instance != null)
+            instance.Init(data);
+        
+        return instance;
     }
 
-    /// <summary>
-    /// EDITOR ONLY!
-    /// 
-    /// Instantiates a packed prefab
-    /// </summary>
-    /// <returns></returns>
-    public UnityEngine.Object InstantiatePrefabObject(Transform parent)
-    {
-        return PrefabUtility.InstantiatePrefab(GameObject, parent);
-    }
+    public T Instantiate(D data) => Initialize(InstantiateInternal(), data);
+    public T Instantiate(Vector3 position, Quaternion rotation, D data) => Initialize(InstantiateInternal(position, rotation), data);
+    public T Instantiate(Vector3 position, Quaternion rotation, Transform parent, D data) => Initialize(InstantiateInternal(position, rotation, parent), data);
+    public T Instantiate(Transform parent, D data) => Initialize(InstantiateInternal(parent), data);
+    public T Instantiate(Transform parent, bool worldPositionStays, D data) => Initialize(InstantiateInternal(parent, worldPositionStays), data);
+    public T Instantiate(InstantiateParameters parameters, D data) => Initialize(InstantiateInternal(parameters), data);
+    public T Instantiate(Vector3 position, Quaternion rotation, InstantiateParameters parameters, D data) => Initialize(InstantiateInternal(position, rotation, parameters), data);
 
-    /// <summary>
-    /// EDITOR ONLY!
-    /// 
-    /// Instantiates a packed prefab
-    /// </summary>
-    /// <returns></returns>
-    public T InstantiatePrefab()
-    {
-        var instance = PrefabUtility.InstantiatePrefab(GameObject) as GameObject;
-        return instance != null ? instance.GetComponent<T>() : null;
-    }
-
-    /// <summary>
-    /// EDITOR ONLY!
-    /// 
-    /// Instantiates a packed prefab
-    /// </summary>
-    /// <returns></returns>
-    public T InstantiatePrefab(Scene destinationScene)
-    {
-        var instance = PrefabUtility.InstantiatePrefab(GameObject, destinationScene) as GameObject;
-        return instance != null ? instance.GetComponent<T>() : null;
-    }
-
-    /// <summary>
-    /// EDITOR ONLY!
-    /// 
-    /// Instantiates a packed prefab
-    /// </summary>
-    /// <returns></returns>
-    public T InstantiatePrefab(Transform parent)
-    {
-        var instance = PrefabUtility.InstantiatePrefab(GameObject, parent) as GameObject;
-        return instance != null ? instance.GetComponent<T>() : null;
-    }
-
+#if UNITY_EDITOR
+    public T InstantiatePrefab(D data) => Initialize(InstantiatePrefabInternal(), data);
+    public T InstantiatePrefab(Scene destinationScene, D data) => Initialize(InstantiatePrefabInternal(destinationScene), data);
+    public T InstantiatePrefab(Transform parent, D data) => Initialize(InstantiatePrefabInternal(parent), data);
 #endif
 }
