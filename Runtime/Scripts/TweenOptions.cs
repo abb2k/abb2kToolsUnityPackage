9562e1b90bb6881c1b7952bc3835e4cef6828182
abb2k <75845401+abb2k@users.noreply.github.com>
2026-07-01 16:07:26 +0300

    moved duration

--- Runtime/Scripts/TweenOptions.cs
+++ Runtime/Scripts/TweenOptions.cs
@@ -43,17 +43,6 @@ namespace Abb2kTools
 #if ODIN_INSPECTOR
         [
             FoldoutGroup(groupName: GENERAL_GROUP_NAME, GroupName = GENERAL_DISPLAY_GROUP_NAME),
-            FoldoutGroup(GENERAL_GROUP_NAME + "/" + OPTIONS_GROUP_NAME),
-            MinValue(0)
-        ]
-#else
-        [Min(0)]
-#endif
-        [Header("General")]
-        public float duration = 0;
-#if ODIN_INSPECTOR
-        [
-            FoldoutGroup(groupName: GENERAL_GROUP_NAME, GroupName = GENERAL_DISPLAY_GROUP_NAME),
             FoldoutGroup(GENERAL_GROUP_NAME + "/" + OPTIONS_GROUP_NAME)
         ]
 #endif
